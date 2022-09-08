604c076ee43af8ff80712b92c4ad136b4d4e98dd
Bart Brouns <bart@magnetophon.nl>
2022-09-08 18:09:05 +0200

    no more allocation on the audio thread
    use the pitch-detection without BufferPool<T>
    
    see: https://github.com/magnetophon/pitch-detection/commit/d17849443641320262b69162435dadf179c97854
    
    less AOTAT
    
    allocations on the audio thread
    
    less AOTAT
    
    no more allocation on the audio thread

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use atomic_float::AtomicF32;
 use nih_plug::prelude::*;
 use nih_plug_vizia::ViziaState;
 use std::sync::Arc;
+use pitch_detection::detector::mcleod::McLeodDetector;
+use pitch_detection::detector::internals::Pitch;
 
 mod editor;
 mod pitch;
@@ -28,7 +30,8 @@ pub struct VoiceMaster {
     signal_index: usize,
     pitch_val: [f32; 2],
     previous_saw : f32,
-
+    optional_pitch: Option<Pitch<f32>>,
+    detector : McLeodDetector<f32>,
 }
 
 #[derive(Params)]
@@ -58,7 +61,8 @@ impl Default for VoiceMaster {
             signal_index: 0,
             pitch_val: [-1.0, 0.0],
             previous_saw :  0.0,
-
+            optional_pitch : None::<Pitch<f32>>,
+            detector : McLeodDetector::new(2048, 1024),
         }
     }
 }
@@ -168,18 +172,19 @@ impl Plugin for VoiceMaster {
                     if self.signal_index == self.signal.len() {
                         self.signal_index = 0;
                         // call the pitchtracker
-                        self.pitch_val = pitch::pitch(self.sample_rate, &self.signal);
-                        nih_trace!(
-                            "Sample Rate: {}, Frequency: {}, Clarity: {}",
-                            self.sample_rate, self.pitch_val[0], self.pitch_val[1]
-                        );
+                        // self.pitch_val = pitch::pitch(self.sample_rate, &self.signal);
+                        self.pitch_val = pitch::pitch(self.sample_rate, &self.signal, &mut self.optional_pitch, self.pitch_val, &mut self.detector);
+                        // nih_trace!(
+                        // "Sample Rate: {}, Frequency: {}, Clarity: {}",
+                        // self.sample_rate, self.pitch_val[0], self.pitch_val[1]
+                        // );
 
 
                     }
                 }
                 match channel_counter {
-                    // 0 => *sample = audio,
-                    1 => // saw at 1/4 freq
+                    // 0 => audio,
+                    1 => // positive saw at 1/4 freq, see https://github.com/magnetophon/VoiceOfFaust/blob/V1.1.4/lib/master.lib#L8
                     {
                         *sample = self.previous_saw+(self.pitch_val[0]/(self.sample_rate*4.0));
                         *sample -= (*sample).floor();
@@ -232,5 +237,5 @@ impl Vst3Plugin for VoiceMaster {
     const VST3_CATEGORIES: &'static str = "Fx|Dynamics";
 }
 
-nih_export_clap!(VoiceMaster);
-nih_export_vst3!(VoiceMaster);
+// nih_export_clap!(VoiceMaster);
+// nih_export_vst3!(VoiceMaster);
