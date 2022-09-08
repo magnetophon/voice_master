c1e59acc9b58afe67cb60256fb2140f898ec0e40
Bart Brouns <bart@magnetophon.nl>
2022-09-08 18:09:05 +0200

    no more allocation on the audio thread
    use the pitch-detection without BufferPool<T>
    
    see: https://github.com/magnetophon/pitch-detection/commit/d17849443641320262b69162435dadf179c97854
    
    less AOTAT
    
    allocations on the audio thread
    
    less AOTAT
    
    no more allocation on the audio thread

--- src/pitch.rs
+++ src/pitch.rs
@@ -1,7 +1,9 @@
 use pitch_detection::detector::mcleod::McLeodDetector;
 use pitch_detection::detector::PitchDetector;
+use pitch_detection::detector::internals::Pitch;
 
-pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>) -> [f32; 2] {
+pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>, optional_pitch: &mut Option<Pitch<f32>>, mut pitch_val: [f32; 2], detector: &mut McLeodDetector<f32>) -> [f32; 2] {
+    // pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>) -> [f32; 2] {
     // Include only notes that exceed a power threshold which relates to the
     // amplitude of frequencies in the signal. Use the suggested default
     // value of 5.0 from the library.
@@ -13,20 +15,22 @@ pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>) -> [f32; 2] {
     // (valid values are in the range 0-1).
     const CLARITY_THRESHOLD: f32 = 0.0;
 
-    let size = signal.len() as usize;
+    // let size = signal.len() as usize;
 
-    let padding = size / 2 as usize;
+    // let padding = size / 2 as usize;
 
-    let mut detector = McLeodDetector::new(size, padding);
+    // let mut detector = McLeodDetector::new(size, padding);
 
-    let optional_pitch = detector.get_pitch(
+
+
+    *optional_pitch = detector.get_pitch(
         &signal,
         sample_rate as usize,
         POWER_THRESHOLD,
         CLARITY_THRESHOLD,
     );
 
-    let mut pitch_val: [f32; 2] = [-1.0, 0.0];
+    // let mut pitch_val: [f32; 2] = [-1.0, 0.0];
 
     match optional_pitch {
         Some(pit) => {
