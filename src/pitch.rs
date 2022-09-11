72cece8e35aceafac09de1a9ae41ef06d956cdda
Bart Brouns <bart@magnetophon.nl>
2022-09-11 18:12:15 +0200

    remove optional_pitch from struct

--- src/pitch.rs
+++ src/pitch.rs
@@ -1,6 +1,5 @@
 use pitch_detection::detector::mcleod::McLeodDetector;
 use pitch_detection::detector::PitchDetector;
-use pitch_detection::detector::internals::Pitch;
 
 // pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>, optional_pitch: &mut Option<Pitch<f32>>, mut pitch_val: [f32; 2], detector: &mut McLeodDetector<f32>) -> [f32; 2] {
 pub(crate) fn pitch(sample_rate: f32, signal: &Vec<f32>, detector: &mut McLeodDetector<f32>) -> [f32; 2] {
