4fec4b3f5d04426a36b4cb488ad02010e3a02594
Bart Brouns <bart@magnetophon.nl>
2023-03-07 13:58:22 +0100

    update dep versions

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ in with nixpkgs;
     name = "moz_overlay_shell";
     nativeBuildInputs = [ pkg-config clang lld ];
     buildInputs = [
-      (nixpkgs.rustChannelOf { date = "2022-07-02"; channel = "nightly"; }).rust
+      (nixpkgs.rustChannelOf { date = "2023-03-07"; channel = "nightly"; }).rust
       rustup
       pkg-config
 
