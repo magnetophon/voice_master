b44d2d1b9897f09f21f9ba086c68018d81447a95
Bart Brouns <bart@magnetophon.nl>
2023-03-08 16:58:46 +0100

    update upstream

--- src/editor.rs
+++ src/editor.rs
@@ -21,7 +21,7 @@ impl Model for Data {}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::from_size(350, 600, false)
+    ViziaState::new(|| (350, 600))
 }
 
 pub(crate) fn create(
